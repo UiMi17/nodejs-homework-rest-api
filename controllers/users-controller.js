177003d7454e59aa609b8998742be43718fd4c3d
Dan Dudaiev <122373927+UiMi17@users.noreply.github.com>
2023-07-11 19:27:18 +0300

    ADDED: Gravatar auto avatar, FIXED: Renamed auth = users for file names

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,11 +1,17 @@
 const User = require("../models/contacts/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const fs = require("fs/promises");
+const jimp = require("jimp");
+const path = require("path");
 
 const { ctrlWrapper } = require("../decorators");
 const { HttpError } = require("../helpers");
 
 const { SECRET_KEY } = process.env;
+
+const avatarDir = path.resolve("public", "avatars");
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -15,9 +21,15 @@ const signup = async (req, res) => {
     throw HttpError(409, "Email in use");
   }
 
+  const avatar = gravatar.url(email, { s: "200" });
+
   const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    avatarURL: avatar,
+    password: hashPassword,
+  });
 
   res.status(201).json({
     user: {
@@ -74,9 +86,29 @@ const logout = async (req, res) => {
   res.status(204).send();
 };
 
+const avatars = async (req, res) => {
+  const { path: oldPath, filename } = req.file;
+
+  const newPath = path.join(avatarDir, filename);
+  await fs.rename(oldPath, newPath);
+
+  const jimpImg = await jimp.read(newPath);
+  await jimpImg.resize(250, 250).write(newPath);
+
+  const avatarURL = path.join("avatars", filename);
+
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    message: avatarURL,
+  });
+};
+
 module.exports = {
   signup: ctrlWrapper(signup),
   signin: ctrlWrapper(signin),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  avatars: ctrlWrapper(avatars),
 };
